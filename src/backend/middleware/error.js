0dbfee79b8e9e99440027d18551c74a47a82df0f
Satish Jatiwal <satish.s.jatiwal@gmail.com>
2023-10-22 10:12:40 +0530

    namechange

--- src/backend/middleware/error.js
+++ src/backend/middleware/error.js
@@ -1,4 +1,4 @@
-const ErrorHandler = require("../utils/errorhandler");
+const ErrorHandler = require("../utils/errorHandler");
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
